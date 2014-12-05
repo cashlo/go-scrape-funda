0d93ab6f72a91b3cb711b2733ca5b690515fd49b
Cash T.H. Lo <tinghin.lo@booking.com>
2014-12-05 17:37:31 +0100

    write to CSV

--- main.go
+++ main.go
@@ -2,35 +2,43 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"os"
 	"regexp"
 	"strconv"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/atotto/encoding/csv"
 )
 
 type House struct {
-	address string
-	price   string
-	link    string
-	area    int
-	volume  int
-	year    int
+	Price   int
+	Address string
+	Link    string
+	Area    int
+	Volume  int
+	Year    int
 }
 
 func ScrapeFunda(url string, result *[]House) {
 	fmt.Printf("Scraping %s\n", url)
 	doc, err := goquery.NewDocument(url)
 	if err != nil {
-		log.Fatal(err)
+		return
 	}
 
+	numberRegex, _ := regexp.Compile("[0-9\\.]+")
+	notNumberRegex, _ := regexp.Compile("[^0-9]")
+
 	doc.Find(".nvm").Each(func(i int, s *goquery.Selection) {
+
 		var h House
-		h.address = strings.TrimSpace(s.Find(".object-street").Text())
-		h.link, _ = s.Find(".object-street").Attr("href")
-		h.price = s.Find(".price").Text()
+		h.Address = strings.TrimSpace(s.Find(".object-street").Text())
+		h.Link, _ = s.Find(".object-street").Attr("href")
+
+		firstPrice := numberRegex.FindString(s.Find(".price").Text())
+		priceString := notNumberRegex.ReplaceAllString(firstPrice, "")
+		h.Price, _ = strconv.Atoi(priceString)
 
 		GetHouseDetail(&h)
 
@@ -39,24 +47,31 @@ func ScrapeFunda(url string, result *[]House) {
 }
 
 func GetHouseDetail(h *House) {
-	url := "http://www.funda.nl" + h.link + "kenmerken/"
+	url := "http://www.funda.nl" + h.Link + "kenmerken/"
 
 	fmt.Printf("Scraping %s\n", url)
 	doc, err := goquery.NewDocument(url)
 	if err != nil {
-		log.Fatal(err)
+		return
 	}
 	yearRegex, _ := regexp.Compile("[0-9]{4}")
 	numberRegex, _ := regexp.Compile("[0-9]+")
-	h.year, _ = strconv.Atoi(yearRegex.FindString(doc.Find("#boja12").Text()))
-	h.area, _ = strconv.Atoi(numberRegex.FindString(doc.Find("#twwo13").Text()))
-	h.volume, _ = strconv.Atoi(numberRegex.FindString(doc.Find("#twih12").Text()))
+
+	h.Year, _ = strconv.Atoi(yearRegex.FindString(doc.Find("#boja12").Text()))
+	h.Area, _ = strconv.Atoi(numberRegex.FindString(doc.Find("#twwo13").Text()))
+	h.Volume, _ = strconv.Atoi(numberRegex.FindString(doc.Find("#twih12").Text()))
 }
 
 func main() {
 	var results []House
-	for i := 1; i <= 1; i++ {
-		ScrapeFunda(fmt.Sprintf("http://www.funda.nl/koop/amsterdam/0-200000/3+kamers/p%d", i), &results)
+	for i := 1; i <= 5; i++ {
+		ScrapeFunda(fmt.Sprintf("http://www.funda.nl/koop/amsterdam/p%d", i), &results)
 	}
+
+	f, _ := os.Create("house.txt")
+	defer f.Close()
+
+	w := csv.NewWriter(f)
+	w.WriteStructAll(results)
 	fmt.Println(results)
 }
